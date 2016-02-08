7776f3ebd9557dbfffceeec3312b77f1eb971288
Daniel Elsberry (RIT Student) <dwe5866@ad.rit.edu>
2016-02-08 14:48:55 -0500

    Added Jumping and started working on movement.

--- CasualGames1/Assets/scripts/PlayerMovement.cs
+++ CasualGames1/Assets/scripts/PlayerMovement.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-
+    public float jumpForce;
+    public Rigidbody rb;
 	// Use this for initialization
 	void Start () {
+        rb = GetComponent<Rigidbody>();
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("Space was pressed");
+            Jump();
+            
+        }	
+    }
+
+    void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce);
+        Debug.Log("Jumping");
+    }
 }
