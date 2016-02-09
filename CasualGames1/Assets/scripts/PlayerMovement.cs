0cad4b413ce4b7f67c600d3f3750e62b509b78c4
Daniel Elsberry (RIT Student) <dwe5866@ad.rit.edu>
2016-02-09 16:15:17 -0500

    Added more platforms to test stage, jumping only works when it's supposed to, movement works... More or less

--- CasualGames1/Assets/scripts/PlayerMovement.cs
+++ CasualGames1/Assets/scripts/PlayerMovement.cs
@@ -2,29 +2,65 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-    public float jumpForce;
+    private float jumpForce;
+    private float moveForce;
     public Rigidbody rb;
+    private bool jumping;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
+        jumping = false;
+        jumpForce = 400;
+        moveForce = 10;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            Debug.Log("Space was pressed");
             Jump();
-            
-        }	
+        }
+        Move();
+        fallCheck();
     }
 
     void Jump()
     {
-        rb.AddForce(transform.up * jumpForce);
-        Debug.Log("Jumping");
+        if (!jumping)
+        {
+            rb.AddForce(transform.up * jumpForce);
+            Debug.Log("Jumping");
+            jumping = true;
+        }
     }
 
     void Move()
-    { }
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(transform.right * moveForce * -1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * moveForce);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Platform")
+        {
+            jumping = false;
+            Debug.Log("able to jump");
+        }
+    }
+
+    void fallCheck()
+    {
+        if (rb.velocity.y < -1)
+        {
+            jumping = true;
+            Debug.Log("falling!");
+        }
+    }
 }
