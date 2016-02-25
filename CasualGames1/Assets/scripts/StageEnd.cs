4dcbb13f3bae2dd17c99b6c650cd4ee081e781c9
Daniel Martin (RIT Student) <dcm5659@ad.rit.edu>
2016-02-25 14:58:07 -0500

    Added: Script to teleport player when they reach the end removed broken script from flare prefab

---
+++ CasualGames1/Assets/scripts/StageEnd.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageEnd : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 startPos;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Capsule");
+		//reference to the players rigidbody
+		rb = player.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		//collision between player and end object
+		if (col.gameObject == player) {
+			//sets the velocity to 0
+			rb.velocity = Vector3.zero;
+			//teleports the player to its start position(set manually)
+			player.transform.position = startPos;
+		}
+	}
+}
